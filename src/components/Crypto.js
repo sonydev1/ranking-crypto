bae72ec06893b6906fb595d355a3b67dfcd35672
Victor Padilla <evictor194@gmail.com>
2022-04-25 02:10:53 -0500

    inicio del proyecto

---
+++ src/components/Crypto.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Crypto = () => {
+    /**
+    * Seater los hooks
+    */
+
+    /**
+     * Funcion para traer los datos  
+     */
+
+    /**
+     * Funcio de busqueda 
+     */
+
+    /**
+     * Filtar los datos  
+     */
+
+    /**
+     * Renderisamos la vista de los compoenetes
+     */
+    return (
+        <div>Crypto</div>
+    )
+}
+
+export default Crypto
